d0be27c62788a32e30ee8aecbfca8a09e5c5c03d
jlobrant <jean.brant@gmail.com>
2022-09-23 11:28:40 -0300

    Setup

---
+++ scripts/setupml.sh
@@ -0,0 +1,19 @@
+# <az_ml_install>
+# az extension add -n ml -y
+# </az_ml_install>
+
+# rc install - uncomment and adjust below to run all tests on a CLI release candidate
+# az extension remove -n ml
+
+# Use a daily build
+az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.8.0-py3-none-any.whl --yes
+ 
+# <set_variables>
+GROUP="rg-ml-mlopsworkspaces-jb"
+LOCATION="eastus2"
+WORKSPACE="mlopsdemojb01"
+# </set_variables>
+
+# <az_configure_defaults>
+az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
+# </az_configure_defaults>
